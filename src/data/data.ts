d884b3cb3113ae2ec2d0cb56101c5097d3e8b6ea
Fauzi Achmad <fachmadb@gmail.com>
2023-07-21 10:56:42 +0700

    edit

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,7 +1,7 @@
 export const spkluList: GeoJSON.FeatureCollection = {
     "type": "FeatureCollection",
     "features": [
-    { "type": "Feature", "properties": { "Name": "Dauh Puri, West Denpasar" }, "geometry": { "type": "Point", "coordinates": [ 115.2177603, -8.6629235 ] } },
+    { "type": "Feature", "properties": { "Name": "Dauh Puri, West Denpasarss" }, "geometry": { "type": "Point", "coordinates": [ 115.2177603, -8.6629235 ] } },
     { "type": "Feature", "properties": { "Name": "Panjer, East Denpasar" }, "geometry": { "type": "Point", "coordinates": [ 115.2430207, -8.6686432 ] } },
     { "type": "Feature", "properties": { "Name": "Benoa, South Kuta" }, "geometry": { "type": "Point", "coordinates": [ 115.2268812, -8.8030039 ] } },
     { "type": "Feature", "properties": { "Name": "Sanur, South Denpasar" }, "geometry": { "type": "Point", "coordinates": [ 115.2599978, -8.7050501 ] } },
